64ca9ca78255a507a01ba5a6e1874ee904cc0d95
Gary Ewan Park <gep13@gep13.co.uk>
2019-07-29 14:22:13 +0100

    Update InstallRequiredApplications.ps1

--- shell/InstallRequiredApplications.ps1
+++ shell/InstallRequiredApplications.ps1
@@ -2,30 +2,44 @@ choco upgrade visualstudio2019community -y --no-progress
 choco upgrade visualstudio2019-workload-manageddesktop -y --no-progress
 choco upgrade visualstudio2019-workload-netweb -y --no-progress
 choco upgrade visualstudio2019-workload-manageddesktop -y --no-progress
+choco upgrade visualstudio2019-workload-azure -y --no-progress
 choco upgrade baretail -y --no-progress
 choco upgrade dotnetcore -y --no-progress
 choco upgrade dotnetcore-sdk -y --no-progress
 choco upgrade dotnet4.7.2 -y --no-progress
-choco install vscode-csharp -y --no-progress
-choco install vscode-csharpextensions -y --no-progress
-choco install vscode-cake -y --no-progress
-choco install cakerecipe-vscode -y --no-progress
-choco install chocolatey-vscode -y --no-progress
-choco install ci-cd-assets-vscode -y --no-progress
-choco install vscode-codespellchecker -y --no-progress
-choco install vscode-editorconfig -y --no-progress
-choco install vscode-markdownlint -y --no-progress
-choco install vscode-gitignore -y --no-progress
-choco install vscode-gitattributes -y --no-progress
-choco install vscode-powershell -y --no-progress
-choco install vscode-icons -y --no-progress
-choco install vscode-autohotkey -y --no-progress
-choco install googlechrome -y --no-progress
-choco install git -y --no-progress
-choco install gitextensions -y --no-progress
-choco install poshgit -y --no-progress
-choco install kdiff3 -y --no-progress
-choco install ChocolateyGUI -y --no-progress
+
+choco upgrade cakerecipe-vscode -y --no-progress
+choco upgrade chocolatey-vscode -y --no-progress
+choco upgrade ci-cd-assets-vscode -y --no-progress
+
+choco upgrade vscode-autohotkey -y --no-progress
+choco upgrade vscode-cake -y --no-progress
+choco upgrade vscode-codespellchecker -y --no-progress
+choco upgrade vscode-csharp -y --no-progress
+choco upgrade vscode-csharpextensions -y --no-progress
+choco upgrade vscode-editorconfig -y --no-progress
+choco upgrade vscode-gitattributes -y --no-progress
+choco upgrade vscode-gitignore -y --no-progress
+choco upgrade vscode-icons -y --no-progress
+choco upgrade vscode-markdownlint -y --no-progress
+choco upgrade vscode-powershell -y --no-progress
+choco upgrade vscode.template -y --no-progress
+choco upgrade vscode-vsliveshare -y --no-progress
+
+choco upgrade googlechrome -y --no-progress
+choco upgrade git -y --no-progress
+choco upgrade gitextensions -y --no-progress
+choco upgrade poshgit -y --no-progress
+choco upgrade kdiff3 -y --no-progress
+choco upgrade ChocolateyGUI -y --no-progress
+choco upgrade microsoftazurestorageexplorer -y --no-progress
+choco upgrade sql-server-express -y --no-progress
+choco upgrade sql-server-management-studio -y --no-progress
+choco upgrade yo -y --no-progress
+choco upgrade msi.template -y --no-progress
+choco upgrade zip.template -y --no-progress
+choco upgrade postman -y --no-progress
+choco upgrade nodejs -y --no-progress
 
 #Update-SessionEnvironment
 #choco install gep13.gitConfig -y  --no-progress --source="https://www.myget.org/F/gep13/api/v2"
