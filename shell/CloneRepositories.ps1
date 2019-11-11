3d31b2414948ab23207d220bd9db87f7ac7d58f6
Gary Ewan Park <gep13@gep13.co.uk>
2019-11-11 19:02:18 +0000

    (maint) update setup scripts
    Using latest version of VM before blowing it away to start again.

--- shell/CloneRepositories.ps1
+++ shell/CloneRepositories.ps1
@@ -1,73 +1,21 @@
 mkdir c:/temp
-mkdir c:/github/organisations/gep13-oss
-mkdir c:/github/organisations/chocolatey
-mkdir c:/github/organisations/GitTools
-mkdir c:/github/organisations/cake-contrib
 mkdir c:/github/organisations/cake-build
+mkdir c:/github/organisations/cake-contrib
+mkdir c:/github/organisations/chocolatey
+mkdir c:/github/organisations/gep13-oss
+mkdir c:/github/organisations/GitTools
 mkdir c:/github/organisations/Homebrew
 mkdir c:/github/organisations/Microsoft
 mkdir c:/github/organisations/poshbotio
+mkdir c:/github/people/buthomas
 mkdir c:/github/people/majkinetor
 mkdir c:/github/people/mushanshitiancai
+mkdir c:/github/people/vhatsura
 
-cd c:/github/organisations/gep13-oss
-git clone https://github.com/gep13-oss/live-streams.git
-git clone https://github.com/gep13-oss/clipimg-vscode.git
-git clone https://github.com/gep13-oss/PoshBot.Gitter.Backend.git
-git clone https://github.com/gep13-oss/PoshBot.Soon.git
-
-cd c:/github/organisations/chocolatey
-git clone https://github.com/gep13/choco.git
-cd choco
-git remote add upstream https://github.com/chocolatey/choco.git
-
-cd c:/github/organisations/GitTools
-git clone https://github.com/gep13/GitReleaseManager.git
-cd GitReleaseManager
-git remote add upstream https://github.com/GitTools/GitReleaseManager.git
-
-cd c:/github/organisations/cake-contrib
-git clone https://github.com/gep13/Cake.DotNetVersionDetector.git
-cd Cake.DotNetVersionDetector
-git remote add upstream https://github.com/cake-contrib/Cake.DotNetVersionDetector.git
-cd ..
-
-git clone https://github.com/gep13/Cake.Ember.git
-cd Cake.Ember
-git remote add upstream https://github.com/cake-contrib/Cake.Ember.git
-cd ..
-
-git clone https://github.com/gep13/Cake.Chocolatey.Module.git
-cd Cake.Chocolatey.Module
-git remote add upstream https://github.com/cake-contrib/Cake.Chocolatey.Module.git
-cd ..
-
-git clone https://github.com/gep13/Cake.Coveralls.git
-cd Cake.Coveralls
-git remote add upstream https://github.com/cake-contrib/Cake.Coveralls.git
-cd ..
-
-git clone https://github.com/gep13/Cake.Gitter.git
-cd Cake.Gitter
-git remote add upstream https://github.com/cake-contrib/Cake.Gitter.git
-cd ..
-
-git clone https://github.com/gep13/Cake.Json.git
-cd Cake.Json
-git remote add upstream https://github.com/cake-contrib/Cake.Json.git
-cd ..
-
-
-git clone https://github.com/gep13/Cake.VsCode.git
-cd Cake.VsCode
-git remote add upstream https://github.com/cake-contrib/Cake.VsCode.git
-cd ..
-
-git clone https://github.com/gep13/Cake.Recipe.git
-cd Cake.Recipe
-git remote add upstream https://github.com/cake-contrib/Cake.Recipe.git
+# Cake Build
 
 cd c:/github/organisations/cake-build
+
 git clone https://github.com/gep13/cake.git
 cd cake
 git remote add upstream https://github.com/cake-build/cake.git
@@ -91,33 +39,193 @@ cd ..
 git clone https://github.com/gep13/website.git
 cd website
 git remote add upstream https://github.com/cake-build/website.git
+cd ..
+
+# Cake Contrib
+
+cd c:/github/organisations/cake-contrib
+
+git clone https://github.com/gep13/Cake.Chocolatey.Module.git
+cd Cake.Chocolatey.Module
+git remote add upstream https://github.com/cake-contrib/Cake.Chocolatey.Module.git
+cd ..
+
+git clone https://github.com/gep13/Cake.Coveralls.git
+cd Cake.Coveralls
+git remote add upstream https://github.com/cake-contrib/Cake.Coveralls.git
+cd ..
+
+git clone https://github.com/gep13/Cake.DotNetCoreEf.git
+cd Cake.DotNetCoreEf
+git remote add upstream https://github.com/cake-contrib/Cake.DotNetCoreEf.git
+cd ..
+
+git clone https://github.com/gep13/Cake.DotNetVersionDetector.git
+cd Cake.DotNetVersionDetector
+git remote add upstream https://github.com/cake-contrib/Cake.DotNetVersionDetector.git
+cd ..
+
+git clone https://github.com/gep13/Cake.Ember.git
+cd Cake.Ember
+git remote add upstream https://github.com/cake-contrib/Cake.Ember.git
+cd ..
+
+git clone https://github.com/gep13/Cake.EntityFramework.git
+cd Cake.EntityFramework
+git remote add upstream https://github.com/cake-contrib/Cake.EntityFramework.git
+cd ..
+
+git clone https://github.com/gep13/Cake.FileHelpers.git
+cd Cake.FileHelpers
+git remote add upstream https://github.com/cake-contrib/Cake.FileHelpers.git
+cd ..
+
+git clone https://github.com/gep13/Cake.FluentMigrator.git
+cd Cake.FluentMigrator
+git remote add upstream https://github.com/cake-contrib/Cake.FluentMigrator.git
+cd ..
+
+git clone https://github.com/gep13/Cake.Gitter.git
+cd Cake.Gitter
+git remote add upstream https://github.com/cake-contrib/Cake.Gitter.git
+cd ..
+
+git clone https://github.com/gep13/Cake.Homebrew.git
+cd Cake.Homebrew
+git remote add upstream https://github.com/cake-contrib/Cake.Homebrew.git
+cd ..
+
+git clone https://github.com/gep13/Cake.Json.git
+cd Cake.Json
+git remote add upstream https://github.com/cake-contrib/Cake.Json.git
+cd ..
+
+git clone https://github.com/gep13/Cake.Karma.git
+cd Cake.Karma
+git remote add upstream https://github.com/cake-contrib/Cake.Karma.git
+cd ..
+
+git clone https://github.com/gep13/Cake.Recipe.git
+cd Cake.Recipe
+git remote add upstream https://github.com/cake-contrib/Cake.Recipe.git
+cd ..
+
+git clone https://github.com/gep13/Cake.SemVer.git
+cd Cake.SemVer
+git remote add upstream https://github.com/cake-contrib/Cake.SemVer.git
+cd ..
+
+git clone https://github.com/gep13/Cake.SqlPackage.git
+cd Cake.SqlPackage
+git remote add upstream https://github.com/cake-contrib/Cake.SqlPackage.git
+cd ..
+
+git clone https://github.com/gep13/Cake.Twitter.git
+cd Cake.Twitter
+git remote add upstream https://github.com/cake-contrib/Cake.Twitter.git
+cd ..
+
+git clone https://github.com/gep13/Cake.VsCode.git
+cd Cake.VsCode
+git remote add upstream https://github.com/cake-contrib/Cake.VsCode.git
+cd ..
+
+# Chocolatey
+
+cd c:/github/organisations/chocolatey
+
+git clone https://github.com/gep13/choco.git
+cd choco
+git remote add upstream https://github.com/chocolatey/choco.git
+cd ..
+
+git clone https://github.com/gep13/ChocolateyGUI.git
+cd ChocolateyGUI
+git remote add upstream https://github.com/chocolatey/ChocolateyGUI.git
+cd ..
+
+# gep13-oss
+
+cd c:/github/organisations/gep13-oss
+
+git clone https://github.com/gep13-oss/clipimg-vscode.git
+git clone https://github.com/gep13-oss/live-streams.git
+git clone https://github.com/gep13-oss/PoshBot.Gitter.Backend.git
+git clone https://github.com/gep13-oss/PoshBot.Soon.git
+
+# GitTools
+
+cd c:/github/organisations/GitTools
+
+git clone https://github.com/gep13/GitReleaseManager.git
+cd GitReleaseManager
+git remote add upstream https://github.com/GitTools/GitReleaseManager.git
+cd ..
+
+# Homebrew
 
 cd c:/github/organisations/Homebrew
+
 git clone https://github.com/gep13/homebrew-core.git
 cd homebrew-core
 git remote add upstream https://github.com/Homebrew/homebrew-core.git
+cd ..
+
+# Microsoft
 
 cd c:/github/organisations/Microsoft
+
 git clone https://github.com/gep13/vscode-azurestorage.git
 cd vscode-azurestorage
 git remote add upstream https://github.com/Microsoft/vscode-azurestorage.git
+cd ..
+
+# PoshBot
 
 cd c:/github/organisations/poshbotio
+
+git clone https://github.com/gep13/PoshBot.Karma.git
+cd PoshBot.Karma
+git remote add upstream https://github.com/poshbotio/PoshBot.Karma.git
+cd ..
+
 git clone https://github.com/gep13/PoshBot.XKCD.git
 cd PoshBot.XKCD
 git remote add upstream https://github.com/poshbotio/PoshBot.XKCD.git
 cd ..
 
-git clone https://github.com/gep13/PoshBot.Karma.git
-cd PoshBot.Karma
-git remote add upstream https://github.com/poshbotio/PoshBot.Karma.git
+# buthomas
+
+cd c:/github/people/buthomas
+
+git clone https://github.com/gep13/Cake.Flyway.git
+cd Cake.Flyway
+git remote add upstream https://github.com/buthomas/Cake.Flyway.git
+cd ..
+
+# Majkinetor
 
 cd c:/github/people/majkinetor
+
 git clone https://github.com/gep13/au.git
 cd au
 git remote add upstream https://github.com/majkinetor/au.git
+cd ..
+
+# Mushanshitiancai
 
 cd c:/github/people/mushanshitiancai
+
 git clone https://github.com/gep13/vscode-paste-image.git
 cd vscode-paste-image
 git remote add upstream https://github.com/mushanshitiancai/vscode-paste-image.git
+cd ..
+
+# vhatsura
+
+cd c:/github/people/vhatsura
+
+git clone https://github.com/gep13/Cake.Systemctl.git
+cd Cake.Systemctl
+git remote add upstream https://github.com/vhatsura/Cake.Systemctl.git
+cd ..
--- shell/InstallRequiredApplications.ps1
+++ shell/InstallRequiredApplications.ps1
@@ -3,62 +3,74 @@ choco upgrade visualstudio2019-workload-manageddesktop -y --no-progress
 choco upgrade visualstudio2019-workload-netweb -y --no-progress
 choco upgrade visualstudio2019-workload-manageddesktop -y --no-progress
 choco upgrade visualstudio2019-workload-azure -y --no-progress
-choco upgrade baretail -y --no-progress
 choco upgrade dotnetcore -y --no-progress
 choco upgrade dotnetcore-sdk -y --no-progress
 choco upgrade dotnet4.7.2 -y --no-progress
 
+choco upgrade vscode-autohotkey -y --no-progress
+choco upgrade vscode-csharp -y --no-progress
+choco upgrade vscode-cake -y --no-progress
 choco upgrade cakerecipe-vscode -y --no-progress
 choco upgrade chocolatey-vscode -y --no-progress
 choco upgrade ci-cd-assets-vscode -y --no-progress
-
-choco upgrade vscode-autohotkey -y --no-progress
-choco upgrade vscode-cake -y --no-progress
 choco upgrade vscode-codespellchecker -y --no-progress
-choco upgrade vscode-csharp -y --no-progress
-choco upgrade vscode-csharpextensions -y --no-progress
 choco upgrade vscode-editorconfig -y --no-progress
 choco upgrade vscode-gitattributes -y --no-progress
 choco upgrade vscode-gitignore -y --no-progress
-choco upgrade vscode-icons -y --no-progress
+choco upgrade vscode-vsliveshare -y --no-progress
 choco upgrade vscode-markdownlint -y --no-progress
 choco upgrade vscode-powershell -y --no-progress
+choco upgrade vscode-icons -y --no-progress
 choco upgrade vscode.template -y --no-progress
-choco upgrade vscode-vsliveshare -y --no-progress
 
-choco upgrade googlechrome -y --no-progress
+choco upgrade baretail -y --no-progress
+choco upgrade ChocolateyGUI -y --no-progress
+choco upgrade dotpeek -y --no-progress
+choco upgrade firefox -y --no-progress
 choco upgrade git -y --no-progress
 choco upgrade gitextensions -y --no-progress
-choco upgrade poshgit -y --no-progress
+choco upgrade googlechrome -y --no-progress
 choco upgrade kdiff3 -y --no-progress
-choco upgrade ChocolateyGUI -y --no-progress
 choco upgrade microsoftazurestorageexplorer -y --no-progress
+choco upgrade msi.template -y --no-progress
+choco upgrade nodejs -y --no-progress
+choco upgrade ngrok -y --no-progress
+choco upgrade poshgit -y --no-progress
+choco upgrade postman -y --no-progress
 choco upgrade sql-server-express -y --no-progress
 choco upgrade sql-server-management-studio -y --no-progress
+choco upgrade transifex-client -y --no-progress
 choco upgrade yo -y --no-progress
-choco upgrade msi.template -y --no-progress
 choco upgrade zip.template -y --no-progress
-choco upgrade postman -y --no-progress
-choco upgrade nodejs -y --no-progress
 
 choco install IIS-WebServer --source windowsfeatures
 choco install IIS-ASPNET45 --source windowsfeatures
 
-#Update-SessionEnvironment
-#choco install gep13.gitConfig -y  --no-progress --source="https://www.myget.org/F/gep13/api/v2"
+Write-Verbose "Install Visual Studio Code Extensions..."
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+code --force --install-extension slevesque.vscode-zipexplorer
+
+Update-SessionEnvironment
+
+# choco install gep13.gitConfig -y  --no-progress --source="https://www.myget.org/F/gep13/api/v2"
 
 # Additional things that should be automated
+# - Install Office - maybe
 # - Remove desktop icons (Edge, Code, Chrome, Git Extensions)
 # - Configure Task bar (Remove Task View, Remove People, Remove Cortana) (Remove pinned Edge, Store, Mail)
 # - Added pinned taskbar item for Administrative PowerShell
 # - Pin Visual Studio to Taskbar
 # - Pin VSCode to Taskbar
 # - Pin Chrome to Taskbar
+# - Pin Excel to Taskbar (if installed)
 # - choco install visualstudio2017-powershelltools -y --no-progress (this needs a 2019 package)
 # - clone cake/secrets repository
 # - Setup PowerShell Profile and Posh-Git
 # - Increase font size in PowerShell
 # - Change to light theme in VSCode
 # - Increase font size in VSCode
-# - Add Environment Variables - GitHubClientId, GitHubClientSecret
+# - Add Environment Variables - GitHubClientId, GitHubClientSecret, GITHUB_PASSWORD, GITHUB_USERNAME
 # - Login to GitHub in browser
+# - Install Azure Storage Emulator
+
+# - Clone secret squirrel projects
